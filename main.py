4ef5acfca3f1a2805f94a07b6c5dc65e9ce02556
collinBlessing <nahuriracollinblessing.com>
2024-08-05 22:40:15 +0300

    applied most of the stuff  waiting to update the file

--- main.py
+++ main.py
@@ -5,7 +5,29 @@ class SetProxy(CTk):
         super().__init__()
         self.geometry('500x300')
         self.title("Set npm proxy")
+
+
+
+
+        # set default properties
+        self.proxy_string = "proxy=http://"
+        self.commented_proxy_string = "#proxy=http://"
+        self.https_proxy_string = "https-proxy=http://"
+        self.commented_https_proxy_string = "#https-proxy=http://"
+
+        self.default_port = StringVar(value="8080")
+
+        # define file title
+        self.file_title = '.npmrc'
+
+        # fetch previous proxy
+        self.previous_proxy_url = self.fetch_previous_proxy()
+
+        # run create widgets function
         self.create_widgets()
+
+
+
 
     def create_widgets(self):
 
@@ -15,28 +37,56 @@ class SetProxy(CTk):
         self.https_proxy_frame = CTkFrame(self, width=100, height=50)
         self.https_proxy_frame.pack()
 
-        # title [proxy]
-        self.proxy_title = CTkLabel(self.proxy_frame, text="proxy")
-        self.proxy_title.pack(side="left" ,pady=20)
+        # title [proxy url title]
+        self.proxy_url_title = CTkLabel(self.proxy_frame, text="url")
+        self.proxy_url_title.pack(side="left" ,pady=20)
 
-        # title [https-proxy]
-        self.proxy_title = CTkLabel(self.https_proxy_frame, text="https-proxy")
-        self.proxy_title.pack(side="left" ,pady=20)
+        # title [proxy port title]
+        self.proxy_port_title = CTkLabel(self.https_proxy_frame, text="port")
+        self.proxy_port_title.pack(side="left" ,pady=20)
 
-        # entry [proxy]
-        self.entry_proxy = CTkEntry(self.proxy_frame, width=200)
-        self.entry_proxy.pack(side="left" , pady=20, padx=20)
+        # entry [proxy url entry]
+        self.proxy_url = CTkEntry(self.proxy_frame, width=200, textvariable=StringVar(value=self.previous_proxy_url ))
+        self.proxy_url.pack(side="left" , pady=20, padx=20)
 
-        # entry [https-proxy]
-        self.entry_proxy = CTkEntry(self.https_proxy_frame, width=200)
-        self.entry_proxy.pack(side="left" , pady=20, padx=20)
+        # entry [proxy port entry]
+        self.proxy_port = CTkEntry(self.https_proxy_frame, width=200, textvariable=self.default_port)
+        self.proxy_port.pack(side="left" , pady=20, padx=20)
 
-        self.button_apply = CTkButton(self, text="Apply", command=self.apply_proxy)
+        self.button_apply = CTkButton(self, text="Apply",fg_color="green", command=self.apply_proxy)
         self.button_apply.pack(pady=20, padx=20)
+
+        self.button_disable = CTkButton(self, text="disable",fg_color="red", command=self.disable_proxy)
+        self.button_disable.pack(pady=20, padx=20)
     
     def apply_proxy(self):
-        proxy_value = self.entry_proxy.get()
-        print(f"Applying npm proxy to: {proxy_value}")
+        val_proxy_url = self.proxy_url.get()
+        proxy_port = self.proxy_port.get()
+        print(f"Applying npm proxy to: {self.proxy_string}{val_proxy_url}:{proxy_port}")
+        print(f"Applying npm https-proxy to: {self.https_proxy_string}{val_proxy_url}:{proxy_port}")
+
+
+    def disable_proxy(self):
+        val_proxy_url = self.proxy_url.get()
+        proxy_port = self.proxy_port.get()
+        print(f"Applying npm proxy to: #{self.proxy_string}{val_proxy_url}:{proxy_port}")
+        print(f"Applying npm https-proxy to: #{self.https_proxy_string}{val_proxy_url}:{proxy_port}")
+
+    def fetch_previous_proxy(self):
+        """Fetches the previous proxy from the specified file.
+        Args:
+            file_path (str, optional): The path to the .npmrc file. Defaults to ".npmrc".
+
+        Returns:
+            str: The previous proxy URL or an empty string if not found.
+        """
+        try:
+            with open(self.file_title, 'r') as npm_proxy_file:
+                previous_port_url = npm_proxy_file.readlines(1)[0].split('//')[1].split(':')[0]
+                return previous_port_url
+        except FileNotFoundError:
+            print("No previous proxy URL")
+
 
 app = SetProxy()
 app.mainloop()
